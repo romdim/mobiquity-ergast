a7f13b9701555bb0602265dd936be8624c33dd3a
romdim <rom-dim@hotmail.com>
2018-05-19 00:48:04 +0300

    Added race winners components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ChampionsComponent } from './champions/champions.component';
+import { WinnersComponent } from './winners/winners.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChampionsComponent
+    ChampionsComponent,
+    WinnersComponent
   ],
   imports: [
     BrowserModule,
