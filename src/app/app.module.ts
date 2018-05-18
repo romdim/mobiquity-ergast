f596f1afc50fbbe404ffbd83a7d906fadd515612
romdim <rom-dim@hotmail.com>
2018-05-19 00:47:31 +0300

    Added champions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ChampionsComponent } from './champions/champions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionsComponent
   ],
   imports: [
     BrowserModule,
