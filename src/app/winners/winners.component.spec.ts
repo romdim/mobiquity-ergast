3485628dbde08224df2c77cea80e44cdba9666a5
romdim <rom-dim@hotmail.com>
2018-05-21 18:10:16 +0300

    Winners initial page with a testhelper

--- src/app/winners/winners.component.spec.ts
+++ src/app/winners/winners.component.spec.ts
@@ -1,25 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 import { WinnersComponent } from './winners.component';
+import { ErgastService } from '../shared/ergast.service';
+import { Champion } from '../shared/models/champion.model';
+import { Winner } from '../shared/models/winner.model';
+import { of } from 'rxjs';
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 
 describe('WinnersComponent', () => {
+  const activatedRoute = new ActivatedRouteStub();
   let component: WinnersComponent;
   let fixture: ComponentFixture<WinnersComponent>;
+  let service: ErgastService;
 
   beforeEach(async(() => {
+    activatedRoute.setParamMap({ season: '2005' });
     TestBed.configureTestingModule({
-      declarations: [ WinnersComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [ WinnersComponent ],
+      providers: [
+        ErgastService,
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(WinnersComponent);
+      component = fixture.componentInstance;
+      service = TestBed.get(ErgastService);
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WinnersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(service).toBeTruthy();
+    expect(service instanceof ErgastService).toBeTruthy();
+  });
+
+  it('should get all winners for 2005 (19)', () => {
+    fixture.detectChanges();
+    expect(component.winners.length).toEqual(19);
+  });
+
+  it('should get all champion for 2005 (1)', () => {
+    fixture.detectChanges();
+    expect(component.champion.driver.getFullName()).toEqual('Fernando Alonso');
   });
 });
--- src/app/winners/winners.component.ts
+++ src/app/winners/winners.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Winner } from '../shared/models/winner.model';
+import { ErgastService } from '../shared/ergast.service';
+import { Champion } from '../shared/models/champion.model';
 
 @Component({
   selector: 'app-winners',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./winners.component.css']
 })
 export class WinnersComponent implements OnInit {
+  champion: Champion;
+  winners: Winner[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private ergastService: ErgastService
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(pmap => this.getChampion(pmap.get('season')));
+    this.route.paramMap.subscribe(pmap => this.getWinners(pmap.get('season')));
   }
 
+  getChampion(season: string): void {
+    this.ergastService.getChampion(season)
+      .subscribe(champion => this.champion = champion);
+  }
+
+  getWinners(season: string): void {
+    this.ergastService.getWinners(season)
+      .subscribe(winners => this.winners = winners);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
