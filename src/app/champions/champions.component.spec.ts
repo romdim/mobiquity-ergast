f52884737ed82942a77107fd753b1f5c462c4e02
romdim <rom-dim@hotmail.com>
2018-05-21 18:09:45 +0300

    Champions initial page

--- src/app/champions/champions.component.spec.ts
+++ src/app/champions/champions.component.spec.ts
@@ -1,25 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ChampionsComponent } from './champions.component';
+import { ErgastService } from '../shared/ergast.service';
+import { Champion } from '../shared/models/champion.model';
 
 describe('ChampionsComponent', () => {
   let component: ChampionsComponent;
   let fixture: ComponentFixture<ChampionsComponent>;
+  let service: ErgastService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChampionsComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [ ChampionsComponent ],
+      providers: [
+        ErgastService
+      ]
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(ChampionsComponent);
+      component = fixture.componentInstance;
+      service = TestBed.get(ErgastService);
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChampionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(service).toBeTruthy();
+    expect(service instanceof ErgastService).toBeTruthy();
+  });
+
+  it('should get all champions from 2005 to 2015 (11)', () => {
+    fixture.detectChanges();
+    expect(component.champions.length).toEqual(11);
   });
 });
--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Champion } from '../shared/models/champion.model';
+import { ErgastService } from '../shared/ergast.service';
+
 @Component({
   selector: 'app-champions',
   templateUrl: './champions.component.html',
   styleUrls: ['./champions.component.css']
 })
 export class ChampionsComponent implements OnInit {
+  champions: Champion[];
 
-  constructor() { }
+  constructor(
+    private ergastService: ErgastService
+  ) { }
 
   ngOnInit() {
+    this.getChampions();
   }
 
+  getChampions(): void {
+    this.ergastService.getChampions()
+      .subscribe(champions => this.champions = champions);
+  }
 }
