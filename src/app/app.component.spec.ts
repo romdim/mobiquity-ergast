ba755fdf7d6b111d794f2735165f7a3dccee464b
romdim <rom-dim@hotmail.com>
2018-05-22 22:26:53 +0300

    Fixed spec for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  MatIconModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { LoaderService } from './shared/loader.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatProgressSpinnerModule,
+        MatIconModule
       ],
       declarations: [
         AppComponent
       ],
-      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        LoaderService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
